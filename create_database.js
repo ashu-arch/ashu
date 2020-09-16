ae3e96ef93ce2085ba0aa57d5e0cbad6ed2cc316
ashu-arch <71361205+ashu-arch@users.noreply.github.com>
2020-09-16 07:33:14 +0530

    Update create_database.js

--- create_database.js
+++ create_database.js
@@ -1,16 +1,15 @@
+module.exports.find_table = function() {
 var mysql = require('mysql');
-
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Ashu@2703"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+
   con.query("CREATE DATABASE mydb", function (err, result) {
     if (err) throw err;
     console.log("Database created");
   });
-});
+
+};
